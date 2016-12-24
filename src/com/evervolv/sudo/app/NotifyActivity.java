f1ce3d610016415f7c49d449cc9199a67e66a0a0
Nick Reuter <nreuter85@gmail.com>
2016-12-23 19:45:57 -0600

    Sudo: we dont use AppCompatActivity
    Change-Id: I077c5b2ea486e3d365036e6ad1e42dacd4a09554

--- src/com/evervolv/sudo/app/NotifyActivity.java
+++ src/com/evervolv/sudo/app/NotifyActivity.java
@@ -16,11 +16,11 @@
 
 package com.evervolv.sudo.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
@@ -28,7 +28,7 @@ import android.widget.TextView;
 
 import com.evervolv.sudo.R;
 
-public class NotifyActivity extends AppCompatActivity {
+public class NotifyActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- src/com/evervolv/sudo/app/RequestActivity.java
+++ src/com/evervolv/sudo/app/RequestActivity.java
@@ -16,11 +16,11 @@
 
 package com.evervolv.sudo.app;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
-public class RequestActivity extends AppCompatActivity {
+public class RequestActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
